5b0199eb05d6ec5115986e36a867af8d5cc71c8a
l0450 <wiktorkumor87@gmail.com>
2025-08-14 11:52:34 +0200

    Objects created

--- Aggregation/src/lib/employeeRegister/Name.java
+++ Aggregation/src/lib/employeeRegister/Name.java
@@ -1,5 +1,69 @@
 package lib.employeeRegister;
 
+/*
+ * A name has a first name and a family name.
+ * There is an option to produce a full name by combining these.
+ * 
+ * @author Luke
+ */
 public class Name {
 
+	// Fields
+	private String firstName;
+	private String familyName;
+	
+	
+	// Constructors
+	public Name() {
+		firstName = "";
+		familyName = "";
+	}
+	
+	public Name(String firstName, String familyName) {
+		this.firstName = firstName;
+		this.familyName = familyName;
+	}
+
+	
+	// Methods
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	public String getFullName() {
+		return firstName + " " + familyName;
+   }
+	
+	
+	@Override
+	public String toString() {
+		return "Name:[firstName=" + firstName + ", familyName=" + familyName + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// Test exceptional cases, i.e. obj not null and is a Name
+		if (obj == null || this.getClass() != obj.getClass())
+			return false;
+
+		Name other = (Name) obj; // Downcast to a Name object
+
+		// Compare first & family names using String' .equals() method
+		return this.familyName.equals(other.familyName)
+			&& this.firstName.equals(other.firstName);
+	}
+	
 }
+
